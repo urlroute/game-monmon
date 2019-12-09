2bee8d2fb254cd4564d1a72a88c2045edd33c285
url route <urlroute@gmail.com>
2019-12-08 20:50:23 -0800

    updated regularJS.js to include correct variable name "pokemonsEl"

--- public/js/components/regularJS.js
+++ public/js/components/regularJS.js
@@ -1,22 +1,13 @@
+var pokemonsEL = document.querySelector('.select-screen')
+  .querySelectorAll('.character')
+console.log(pokemonsEL)
 
+i = 0;
+while (i < pokemonsEL.length) {
+  pokemonsEL[i]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  i++
+}
 
 
 
@@ -42,40 +33,39 @@
 
 
 // 88888888888888888888888888888888   POSSIBLE CHOOSING POKEMONS?????  88888888888888888888888888888888888888888888888888
-var pokemons = [
-    {
-      name: 'charmander',
-      type: 'fire',
-      attack: 52,
-      stamina: 39,
-      level: 1
-    },
-    {
-      name: 'charmander',
-      type: 'fire',
-      attack: 52,
-      stamina: 39,
-      level: 1
-    },
-    
-  ]
-    
-  var attack = 20;
-  var level = 10;
-  var stack = 1.3;
-  var stamina = 39;
-  
-  // create a formula for attacks
-  console.log((attack * level ) * stack / 7)
-   
-  
-  // create a formula for health
-  //HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
-  console.log(((0.20 * Math.sqrt(level)) * stamina) * 15)
- 
-  // let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
-  // p1 vs p2
-   
-  // when one user loses all his health declare a winner
-  
-  // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+var pokemons = [{
+    name: 'charmander',
+    type: 'fire',
+    attack: 52,
+    stamina: 39,
+    level: 1
+  },
+  {
+    name: 'charmander',
+    type: 'fire',
+    attack: 52,
+    stamina: 39,
+    level: 1
+  },
+
+]
+
+var attack = 20;
+var level = 10;
+var stack = 1.3;
+var stamina = 39;
+
+// create a formula for attacks
+console.log((attack * level) * stack / 7)
+
+
+// create a formula for health
+//HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
+console.log(((0.20 * Math.sqrt(level)) * stamina) * 15)
+
+// let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
+// p1 vs p2
+
+// when one user loses all his health declare a winner
+
+// 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
